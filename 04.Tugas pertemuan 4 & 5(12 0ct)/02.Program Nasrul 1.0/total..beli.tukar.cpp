649f11e756f78cf5f5786b9ac9a4fd332fa991f9
rhcczmr <91588457+rhcczmr@users.noreply.github.com>
2021-10-25 11:08:49 +0900

    Alpro

---
+++ 04.Tugas pertemuan 4 & 5(12 0ct)/02.Program Nasrul 1.0/total..beli.tukar.cpp
@@ -0,0 +1,18 @@
+//Program nasrul 1.0
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int uang,jumlah,tukar;
+
+    cout<<"Masukkan uang nsarul  : ";
+    cin>>uang;
+    cout<<"\n";
+    jumlah = uang/250;
+    tukar  = jumlah/3;
+    jumlah = jumlah +tukar;
+
+    cout<<"Total permen yang didapatkan : "<<jumlah<<endl;
+    return 0;
+}
